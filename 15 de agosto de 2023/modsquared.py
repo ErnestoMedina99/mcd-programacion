2a526bffabd3e30492bcd7a034773cb757029735
Ernesto Medina <a217204314@unison.mx>
2023-08-30 19:06:06 -0700

    ffwh

---
+++ 15 de agosto de 2023/modsquared.py
@@ -0,0 +1,11 @@
+"""
+Esta función calcula el modulo al cuadrado de un vector en R2.
+"""
+def modulocuadrado(x, y):
+    """
+    Esta función calcula el modulo al cuadrado de un vector en R2.
+    """
+    
+    mod = x**2+y**2
+    
+    return mod
